e3247070ab378a63c9581e33b73cbedc2229a6db
JosafatJimenezB <josa221001@gmail.com>
2022-04-25 19:02:13 -0500

    add: efecto hover mejorado para submenu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 const submenu = document.querySelector('.submenu-position');
 const menu = document.querySelector('.submenu');
-
-// submenu.addEventListener('click', (e) => {
-//     console.log('click');
-//     menu.classList.add('active');
-// });
+const menu_toggle = document.querySelector('.menu-toggle');
+const close = document.querySelector('.close');
+const toggle = document.querySelector('.toggle');
 
 
-submenu.onmouseover = function () {
-    menu.classList.add('active');
-}
+// On toggle click activate menu
+toggle.addEventListener('click', (e) => {
+    menu_toggle.classList.add('active');
+    close.classList.add('active');
+});
 
-submenu.onmouseout = function () {
+close.addEventListener('click', (e) => {
     menu.classList.remove('active');
-}
+    close.classList.remove('active');
+});
+
+
+
+//On Mouse hover activate submenu
+
+submenu.addEventListener('mouseover', (e) => {
+    menu.classList.add('active');
+});
+
+
+submenu.addEventListener('mouseout', (e) => {
+    menu.classList.remove('active');
+});
+
